3a8a110f6f7f303bbf272c7a7b8b9e46ca96848c
Lanie Arnold <delaynaarnold@outlook.com>
2022-02-10 13:43:07 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
           <div className="col-md-5 m-1">
@@ -42,7 +42,8 @@ function RenderComments({comments, addComment, campsiteId}) {
                 </div>
               );
             })}
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+           <CommentForm campsiteId={campsiteId} postComment={postComment} />
+
           </div>
         );
     }
@@ -68,9 +69,8 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
-    this.props.campsiteId, values.rating, values.author,  values.text );
-  }
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
+}
 
   render() {
     return (
@@ -180,9 +180,10 @@ if (props.campsite) {
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
-            comments={props.comments}
-            addComment={props.addComment}
-            campsiteId={props.campsite.id}/>
+              comments={props.comments}
+              postComment={props.postComment}
+              campsiteId={props.campsite.id}
+                    />                 
         </div>
       </div>
     );
--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -17,3 +17,13 @@ export const Promotions = (state = { isLoading: true,
             return state;
       }
 };
+
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: "Mountain Adventure",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        featured: true,
+        description: "Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided."
+    }
+]
